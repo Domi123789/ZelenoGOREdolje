d7269f24b161c6660cc5dec8a4e66f07b5ee00a3
Ucenik <Ucenik@DESKTOP-EDUBNMJ>
2023-11-08 10:29:37 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace ZelenoGOREdolje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gore = new System.Windows.Forms.Button();
+            this.Desno = new System.Windows.Forms.Button();
+            this.Dolje = new System.Windows.Forms.Button();
+            this.Lijevo = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.min = new System.Windows.Forms.Button();
+            this.max = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Gore
+            // 
+            this.Gore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Gore.Location = new System.Drawing.Point(313, 280);
+            this.Gore.Name = "Gore";
+            this.Gore.Size = new System.Drawing.Size(75, 23);
+            this.Gore.TabIndex = 0;
+            this.Gore.Text = "Gore";
+            this.Gore.UseVisualStyleBackColor = true;
+            this.Gore.Click += new System.EventHandler(this.Gore_Click);
+            // 
+            // Desno
+            // 
+            this.Desno.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Desno.Location = new System.Drawing.Point(161, 341);
+            this.Desno.Name = "Desno";
+            this.Desno.Size = new System.Drawing.Size(75, 23);
+            this.Desno.TabIndex = 1;
+            this.Desno.Text = "Desno";
+            this.Desno.UseVisualStyleBackColor = true;
+            this.Desno.Click += new System.EventHandler(this.Desno_Click);
+            // 
+            // Dolje
+            // 
+            this.Dolje.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Dolje.Location = new System.Drawing.Point(313, 341);
+            this.Dolje.Name = "Dolje";
+            this.Dolje.Size = new System.Drawing.Size(75, 23);
+            this.Dolje.TabIndex = 2;
+            this.Dolje.Text = "Dolje";
+            this.Dolje.UseVisualStyleBackColor = true;
+            this.Dolje.Click += new System.EventHandler(this.Dolje_Click);
+            // 
+            // Lijevo
+            // 
+            this.Lijevo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Lijevo.Location = new System.Drawing.Point(462, 341);
+            this.Lijevo.Name = "Lijevo";
+            this.Lijevo.Size = new System.Drawing.Size(75, 23);
+            this.Lijevo.TabIndex = 3;
+            this.Lijevo.Text = "Lijevo";
+            this.Lijevo.UseVisualStyleBackColor = true;
+            this.Lijevo.Click += new System.EventHandler(this.Lijevo_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(268, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(418, 32);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(547, 32);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(112, 85);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(268, 85);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(418, 85);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 10;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(547, 85);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 11;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(112, 138);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 12;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(268, 138);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 22);
+            this.textBox10.TabIndex = 13;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(418, 138);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 22);
+            this.textBox11.TabIndex = 14;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(547, 138);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 22);
+            this.textBox12.TabIndex = 15;
+            // 
+            // min
+            // 
+            this.min.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.min.Location = new System.Drawing.Point(518, 393);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(75, 23);
+            this.min.TabIndex = 16;
+            this.min.Text = "-";
+            this.min.UseVisualStyleBackColor = true;
+            this.min.Click += new System.EventHandler(this.min_Click);
+            // 
+            // max
+            // 
+            this.max.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.max.Location = new System.Drawing.Point(617, 393);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(75, 23);
+            this.max.TabIndex = 17;
+            this.max.Text = "max";
+            this.max.UseVisualStyleBackColor = true;
+            this.max.Click += new System.EventHandler(this.max_Click);
+            // 
+            // Close
+            // 
+            this.Close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Close.Location = new System.Drawing.Point(713, 393);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 18;
+            this.Close.Text = "X";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Lijevo);
+            this.Controls.Add(this.Dolje);
+            this.Controls.Add(this.Desno);
+            this.Controls.Add(this.Gore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Gore;
+        private System.Windows.Forms.Button Desno;
+        private System.Windows.Forms.Button Dolje;
+        private System.Windows.Forms.Button Lijevo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Button min;
+        private System.Windows.Forms.Button max;
+        private System.Windows.Forms.Button Close;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZelenoGOREdolje
+{
+    public partial class Form1 : Form
+    {
+        int current = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.BackColor = Color.GreenYellow;
+
+        }
+
+        private void Gore_Click(object sender, EventArgs e)
+        {
+            switch (current)
+            {
+                case 1:
+                    textBox9.BackColor = Color.GreenYellow;
+                    textBox1.BackColor = Color.White;
+                    current = 9;
+                    break;
+                case 2:
+                    textBox10.BackColor = Color.GreenYellow;
+                    textBox2.BackColor = Color.White;
+                    current = 10;
+                    break;
+                case 3:
+                    textBox11.BackColor = Color.GreenYellow;
+                    textBox3.BackColor = Color.White;
+                    current = 11;
+                    break;
+                case 4:
+                    textBox12.BackColor = Color.GreenYellow;
+                    textBox4.BackColor = Color.White;
+                    current = 12;
+                    break;
+                case 5:
+                    textBox1.BackColor = Color.GreenYellow;
+                    textBox5.BackColor = Color.White;
+                    current = 1;
+                    break;
+                case 6:
+                    textBox2.BackColor = Color.GreenYellow;
+                    textBox6.BackColor = Color.White;
+                    current = 2;
+                    break;
+                case 7:
+                    textBox3.BackColor = Color.GreenYellow;
+                    textBox7.BackColor = Color.White;
+                    current = 3;
+                    break;
+                case 8:
+                    textBox4.BackColor = Color.GreenYellow;
+                    textBox8.BackColor = Color.White;
+                    current = 4;
+                    break;
+                case 9:
+                    textBox5.BackColor = Color.GreenYellow;
+                    textBox9.BackColor = Color.White;
+                    current = 5;
+                    break;
+                case 10:
+                    textBox6.BackColor = Color.GreenYellow;
+                    textBox10.BackColor = Color.White;
+                    current = 6;
+                    break;
+                case 11:
+                    textBox7.BackColor = Color.GreenYellow;
+                    textBox11.BackColor = Color.White;
+                    current = 7;
+                    break;
+                case 12:
+                    textBox8.BackColor = Color.GreenYellow;
+                    textBox12.BackColor = Color.White;
+                    current = 8;
+                    break;
+                default: break;
+            }
+
+
+        }
+
+        private void Dolje_Click(object sender, EventArgs e)
+        {
+            switch (current)
+            {
+                case 1:
+                    textBox5.BackColor = Color.GreenYellow;
+                    textBox1.BackColor = Color.White;
+                    current = 5;
+                    break;
+                case 2:
+                    textBox6.BackColor = Color.GreenYellow;
+                    textBox2.BackColor = Color.White;
+                    current = 6;
+                    break;
+                case 3:
+                    textBox7.BackColor = Color.GreenYellow;
+                    textBox3.BackColor = Color.White;
+                    current = 7;
+                    break;
+                case 4:
+                    textBox8.BackColor = Color.GreenYellow;
+                    textBox4.BackColor = Color.White;
+                    current = 8;
+                    break;
+                case 5:
+                    textBox9.BackColor = Color.GreenYellow;
+                    textBox5.BackColor = Color.White;
+                    current = 9;
+                    break;
+                case 6:
+                    textBox10.BackColor = Color.GreenYellow;
+                    textBox6.BackColor = Color.White;
+                    current = 10;
+                    break;
+                case 7:
+                    textBox11.BackColor = Color.GreenYellow;
+                    textBox7.BackColor = Color.White;
+                    current = 11;
+                    break;
+                case 8:
+                    textBox12.BackColor = Color.GreenYellow;
+                    textBox8.BackColor = Color.White;
+                    current = 12;
+                    break;
+                case 9:
+                    textBox1.BackColor = Color.GreenYellow;
+                    textBox9.BackColor = Color.White;
+                    current = 1;
+                    break;
+                case 10:
+                    textBox2.BackColor = Color.GreenYellow;
+                    textBox10.BackColor = Color.White;
+                    current = 2;
+                    break;
+                case 11:
+                    textBox3.BackColor = Color.GreenYellow;
+                    textBox11.BackColor = Color.White;
+                    current = 3;
+                    break;
+                case 12:
+                    textBox4.BackColor = Color.GreenYellow;
+                    textBox12.BackColor = Color.White;
+                    current = 4;
+                    break;
+                default: break;
+            }
+
+        }
+
+        private void Desno_Click(object sender, EventArgs e)
+        {
+            switch (current)
+            {
+                case 1:
+                    textBox2.BackColor = Color.GreenYellow;
+                    textBox1.BackColor = Color.White;
+                    current = 2;
+                    break;
+                case 2:
+                    textBox3.BackColor = Color.GreenYellow;
+                    textBox2.BackColor = Color.White;
+                    current = 3;
+                    break;
+                case 3:
+                    textBox4.BackColor = Color.GreenYellow;
+                    textBox3.BackColor = Color.White;
+                    current = 4;
+                    break;
+                case 4:
+                    textBox1.BackColor = Color.GreenYellow;
+                    textBox4.BackColor = Color.White;
+                    current = 1;
+                    break;
+                case 5:
+                    textBox6.BackColor = Color.GreenYellow;
+                    textBox5.BackColor = Color.White;
+                    current = 6;
+                    break;
+                case 6:
+                    textBox7.BackColor = Color.GreenYellow;
+                    textBox6.BackColor = Color.White;
+                    current = 7;
+                    break;
+                case 7:
+                    textBox8.BackColor = Color.GreenYellow;
+                    textBox7.BackColor = Color.White;
+                    current = 8;
+                    break;
+                case 8:
+                    textBox5.BackColor = Color.GreenYellow;
+                    textBox8.BackColor = Color.White;
+                    current = 5;
+                    break;
+                case 9:
+                    textBox10.BackColor = Color.GreenYellow;
+                    textBox9.BackColor = Color.White;
+                    current = 10;
+                    break;
+                case 10:
+                    textBox11.BackColor = Color.GreenYellow;
+                    textBox10.BackColor = Color.White;
+                    current = 11;
+                    break;
+                case 11:
+                    textBox12.BackColor = Color.GreenYellow;
+                    textBox11.BackColor = Color.White;
+                    current = 12;
+                    break;
+                case 12:
+                    textBox9.BackColor = Color.GreenYellow;
+                    textBox12.BackColor = Color.White;
+                    current = 9;
+                    break;
+            default : break;
+            }
+
+
+        }
+
+        private void Lijevo_Click(object sender, EventArgs e)
+        {
+            switch (current)
+            {
+                case 1:
+                    textBox4.BackColor = Color.GreenYellow;
+                    textBox1.BackColor = Color.White;
+                    current = 4;
+                    break;
+                case 2:
+                    textBox1.BackColor = Color.GreenYellow;
+                    textBox2.BackColor = Color.White;
+                    current = 1;
+                    break;
+                case 3:
+                    textBox2.BackColor = Color.GreenYellow;
+                    textBox3.BackColor = Color.White;
+                    current = 2;
+                    break;
+                case 4:
+                    textBox3.BackColor = Color.GreenYellow;
+                    textBox4.BackColor = Color.White;
+                    current = 3;
+                    break;
+                case 5:
+                    textBox8.BackColor = Color.GreenYellow;
+                    textBox5.BackColor = Color.White;
+                    current = 8;
+                    break;
+                case 6:
+                    textBox5.BackColor = Color.GreenYellow;
+                    textBox6.BackColor = Color.White;
+                    current = 5;
+                    break;
+                case 7:
+                    textBox6.BackColor = Color.GreenYellow;
+                    textBox7.BackColor = Color.White;
+                    current = 6;
+                    break;
+                case 8:
+                    textBox7.BackColor = Color.GreenYellow;
+                    textBox8.BackColor = Color.White;
+                    current = 7;
+                    break;
+                case 9:
+                    textBox12.BackColor = Color.GreenYellow;
+                    textBox9.BackColor = Color.White;
+                    current = 12;
+                    break;
+                case 10:
+                    textBox9.BackColor = Color.GreenYellow;
+                    textBox10.BackColor = Color.White;
+                    current = 9;
+                    break;
+                case 11:
+                    textBox10.BackColor = Color.GreenYellow;
+                    textBox11.BackColor = Color.White;
+                    current = 10;
+                    break;
+                case 12:
+                    textBox11.BackColor = Color.GreenYellow;
+                    textBox12.BackColor = Color.White;
+                    current = 11;
+                    break;
+                default: break;
+            }
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void min_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void max_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
+
